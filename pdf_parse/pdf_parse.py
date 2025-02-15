146d9d9788e3cc844a202fa78e6929ba294dc912
JuliaJu <yulia.ovva@gmail.com>
2025-02-16 00:44:32 +0300

    feat(concat_images): Add concatenation of images to pdf
    Add concatenation of images to pdf

--- pdf_parse/pdf_parse.py
+++ pdf_parse/pdf_parse.py
@@ -1,14 +1,14 @@
+from pathlib import Path
 import pymupdf
-import os
 
 
-def convert_pdf_to_images(pdf_path, output_folder, zoom_x=2.0, zoom_y=2.0):
+def convert_pdf_to_images(pdf_path, images_folder, zoom_x=2.0, zoom_y=2.0):
     """
     Конвертирует страницы PDF в изображения и сохраняет их в указанную папку.
 
     Args:
-        pdf_path (str): Путь к PDF-файлу.
-        output_folder (str): Папка для сохранения изображений.
+        pdf_path (str или Path): Путь к PDF-файлу.
+        images_folder (str или Path): Папка для сохранения изображений.
         zoom_x (float): Коэффициент масштабирования по горизонтали. По умолчанию 2.0.
         zoom_y (float): Коэффициент масштабирования по вертикали. По умолчанию 2.0.
 
@@ -17,25 +17,91 @@ def convert_pdf_to_images(pdf_path, output_folder, zoom_x=2.0, zoom_y=2.0):
     """
 
     try:
+        print(f"------Обработка PDF-файла: {pdf_path}------")
+
+        # Создание путей
+        pdf_path = Path(pdf_path)
+        images_folder = Path(images_folder)
+
         # Чтение PDF-документа
         doc = pymupdf.open(pdf_path)
-        print(f"Количество страниц в документе: {len(doc)}")
+        doc_len = len(doc)
 
         # Создание папки для сохранения изображений, если она не существует
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+        images_folder.mkdir(parents=True, exist_ok=True)
 
         # Матрица для масштабирования
         mat = pymupdf.Matrix(zoom_x, zoom_y)
 
-        # Концертациям каждой страницы в изображение
+        # Конвертация каждой страницы в изображение
         for page in doc:
-            pix = page.get_pixmap(matrix=mat)  # С использованием масштабирования matrix=mat
-            output_path = os.path.join(output_folder, f"page-{page.number}.png")
-            pix.save(output_path)
+            pix = page.get_pixmap(matrix=mat)  # Использование масштабирования matrix=mat
+            output_path = images_folder / f"page-{page.number}.png"
+            pix.save(output_path)  
 
-        print(f"Изображения сохранены в папку: {output_folder}")
-        return f"Успешно сохранено {len(doc)} страниц."
+        doc.close()
+
+        print(f"Путь до изображений: /{images_folder}")
+
+        # Проверка количества обработанных страниц
+        images_list = [file.name for file in images_folder.iterdir() if file.is_file()]
+        images_count = len(images_list)
+        if images_count != doc_len:
+            raise ValueError(f"Ожидаемое количество изображений: {doc_len}. Получено: {images_count}")
+        return f"Успешно обработано страниц: {images_count} из {doc_len}"
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+    
+
+
+def convert_images_to_pdf(pdf_path, images_folder):
+    """
+    Конвертирует изображения из указанной папки в PDF-файл.
+
+    Args:
+        pdf_path (str или Path): Путь для сохранения результирующего PDF-файла.
+        images_folder (str или Path): Папка, содержащая изображения для конвертации.
+
+    Returns:
+        str или None: Сообщение об успешном завершении или None в случае ошибки.
+    """
+
+    try:
+        print(f"------Обработка изображений: /{images_folder}------")
+
+        # Создание путей
+        images_folder = Path(images_folder)
+        images_list = [file.name for file in images_folder.iterdir() if file.is_file()]
+        images_count = len(images_list)
+
+        # Создание пустого PDF-документа
+        doc = pymupdf.open()
+
+        # Добавление изображений в PDF-документ
+        for item in images_list:
+
+            # Открытие изображения и его конвертация в PDF
+            img = pymupdf.open(images_folder / item)
+            rect = img[0].rect
+            pdfbytes = img.convert_to_pdf()
+            img.close()
+
+            # Добавление страницы в PDF-документ
+            img_pdf = pymupdf.open("pdf", pdfbytes)
+            page = doc.new_page(width = rect.width,
+                            height = rect.height)
+            page.show_pdf_page(rect, img_pdf, 0)
+
+        doc_len = len(doc)
+
+        # Сохранение PDF-документа
+        doc.save(pdf_path)        
+        doc.close()
+
+        print(f"Обработано изображений: {images_count} из {doc_len} ")        
+        return f"PDF-файл успешно сохранен: {pdf_path}"
 
     except Exception as e:
         print(f"Произошла ошибка: {e}")
@@ -45,7 +111,13 @@ def convert_pdf_to_images(pdf_path, output_folder, zoom_x=2.0, zoom_y=2.0):
 # Пример использования
 if __name__ == "__main__":
     pdf_path = "test.pdf"
-    output_folder = "images"
-    result = convert_pdf_to_images(pdf_path, output_folder)
-    if result:
-        print(result)
+    images_folder = "images"
+    parse_result = convert_pdf_to_images(pdf_path, images_folder)
+    if parse_result:
+        print(parse_result)
+
+    sorted_pdf_path = "sorted_test.pdf"
+    concat_result = convert_images_to_pdf(sorted_pdf_path, images_folder)
+    if concat_result:
+        print(concat_result)
+    
